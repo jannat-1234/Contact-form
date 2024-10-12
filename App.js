3b868f75311b3f3b14a1ebb145063f4cc9bfebea
jannat-1234 <Jannatansari1811@gmail.com>
2024-10-12 18:34:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ContactForm from './Component/ContactForm';
+
+
+function App() {
+  return (
+    
+    <>
+  <ContactForm/>
+    </>
+  );
+}
+
+export default App;
